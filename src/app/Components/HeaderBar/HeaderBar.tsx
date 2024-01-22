33ddb681e9ab3f4bf386437f694247c2f0cd225d
Ivan <94634131+veetsok@users.noreply.github.com>
2024-01-22 09:26:23 +0400

    first

---
+++ src/app/Components/HeaderBar/HeaderBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import * as ST from "./styled/styled";
+
+interface HeaderBarProps {}
+
+const HeaderBar: React.FC<HeaderBarProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default HeaderBar;
