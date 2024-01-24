a4a154e4028ee2c8f51587121519cb658d5493fb
Ivan <94634131+veetsok@users.noreply.github.com>
2024-01-24 17:44:29 +0400

    update

--- src/app/business.InterfaceLayer/hooks/useQueryStore.ts
+++ src/app/business.InterfaceLayer/hooks/useQueryStore.ts
@@ -1,7 +1,4 @@
 "use client";
-
-import { useQueryClient, useQuery } from "react-query";
-
 export interface Products {
   id: number;
   title: string;
@@ -25,21 +22,3 @@ export async function getProducts() {
   const data = await response.json();
   return data;
 }
-
-// const fetchProducts = async () => {
-//   const response = await fetch(`${baseUrl}`);
-//   const data = await response.json();
-//   return data;
-// };
-
-// export const useQueryStore = () => {
-//   const queryClient = useQueryClient();
-
-//   const { data: products, isLoading } = useQuery("products", fetchProducts, {
-//     onSuccess: (data) => {
-//       queryClient.setQueryData("products", data);
-//     },
-//   });
-
-//   return { products, isLoading };
-// };
