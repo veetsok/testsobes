1ad72c6cf67411c0f6085a3e84d38b49ae69989f
Ivan <94634131+veetsok@users.noreply.github.com>
2024-01-26 20:34:47 +0400

    Add ProductPage

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,15 +2,31 @@
 
 import React from "react";
 import styles from "../../styles/page.module.css";
+import { useQuery } from "react-query";
+import { getProducts } from "@/app/business.InterfaceLayer/hooks/useQueryStore";
+import { useParams } from "next/navigation";
+import { Products } from "@/app/business.InterfaceLayer/types";
 
 interface ProductPageProps {}
 
 const ProductPage: React.FC<ProductPageProps> = () => {
+  const { id } = useParams<{ id: string }>();
+  const { data: products, isLoading } = useQuery("products", getProducts);
+  const product = products?.find((product: Products) => product.id === parseInt(id));
+
   return (
     <div className={styles.container}>
       <h1>Product Details Page</h1>
-      <h2>$product.title</h2>
-      <h2>$product.price</h2>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : product ? (
+        <>
+          <h2>{product.title}</h2>
+          <h2>{product.priceSale}</h2>
+        </>
+      ) : (
+        <div>Product not found</div>
+      )}
     </div>
   );
 };
