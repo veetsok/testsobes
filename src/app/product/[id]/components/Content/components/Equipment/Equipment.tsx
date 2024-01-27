be0fe5eca585c21b78927c364e02a694d1ef3bb3
Ivan <94634131+veetsok@users.noreply.github.com>
2024-01-27 21:35:23 +0400

    update

--- src/app/product/[id]/components/Content/components/Equipment/Equipment.tsx
+++ src/app/product/[id]/components/Content/components/Equipment/Equipment.tsx
@@ -8,6 +8,9 @@ import ButtonEnum from "@/app/user.InterfaceLayer/UI_KIT/Atoms/Button.atom/enum"
 import ImageAtom from "@/app/user.InterfaceLayer/UI_KIT/Atoms/Image.atom";
 import ImageEnum from "@/app/user.InterfaceLayer/UI_KIT/Atoms/Image.atom/enum";
 import Colors from "@/app/user.InterfaceLayer/constants/colors";
+import PinkIcon from "../../../../../../user.InterfaceLayer/shared/assets/icons/pink.svg?react";
+import BlueIcon from "../../../../../../user.InterfaceLayer/shared/assets/icons/blue.svg?react";
+import RedIcon from "../../../../../../user.InterfaceLayer/shared/assets/icons/red.svg?react";
 
 interface EquipmentProps {}
 
@@ -238,6 +241,69 @@ const Equipment: React.FC<EquipmentProps> = () => {
           </ST.GiftBox>
         </ST.BlockDefault>
       </ST.GiftBlock>
+      <ST.BlockDefault>
+        <div>
+          <ButtonAtom
+            type={ButtonEnum.enum_equipmentButton}
+            display="inline-block"
+            width="227px"
+            height="126px"
+            padding="27.5px 0"
+            margin="20px 20px 0  0"
+          >
+            <TextAtom
+              type={TextEnum.enum_Text_H4}
+              margin="0 0 -10px 0"
+              color={Colors.BLACK}
+            >
+              Без упаковки
+            </TextAtom>
+          </ButtonAtom>
+          <ButtonAtom
+            type={ButtonEnum.enum_equipmentButton}
+            display="inline-block"
+            width="227px"
+            height="126px"
+            padding="27.5px 0"
+            margin="20px 0 0  0"
+          >
+            <ImageAtom type={ImageEnum.enum_defaultSvg} icon={<PinkIcon />} />
+            <TextAtom type={TextEnum.enum_Text_H4} color={Colors.BLACK}>
+              Розовый
+            </TextAtom>
+          </ButtonAtom>
+          <ButtonAtom
+            type={ButtonEnum.enum_equipmentButton}
+            display="inline-block"
+            width="227px"
+            height="126px"
+            padding="27.5px 0"
+            margin="20px 20px 0  0"
+          >
+            <ImageAtom
+              type={ImageEnum.enum_defaultSvg}
+              fill={Colors.PURPLE}
+              icon={<BlueIcon />}
+            />
+            <TextAtom type={TextEnum.enum_Text_H4} color={Colors.BLACK}>
+              Синий
+            </TextAtom>
+          </ButtonAtom>
+          <ButtonAtom
+            type={ButtonEnum.enum_equipmentButton}
+            display="inline-block"
+            width="227px"
+            height="126px"
+            padding="27.5px 0"
+            margin="20px 20px 0  0"
+          >
+            <ImageAtom type={ImageEnum.enum_defaultSvg} icon={<RedIcon />} />
+            <TextAtom type={TextEnum.enum_Text_H4} color={Colors.BLACK}>
+              Красный
+            </TextAtom>
+          </ButtonAtom>
+        </div>
+      </ST.BlockDefault>
     </ST.Equipment>
   );
 };
